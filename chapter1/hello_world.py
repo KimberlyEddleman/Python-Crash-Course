4294e7e7ce6ba656c9e551d1d995e6aeca7b2754
KimberlyEddleman <47014836+KimberlyEddleman@users.noreply.github.com>
2022-08-01 19:42:39 -0400

    completed chapter 2

---
+++ chapter1/hello_world.py
@@ -0,0 +1,1 @@
+message=print("Hello Python World!")
---
+++ chapter2/chapter2 examples.py
@@ -0,0 +1,77 @@
+# 2-1 Simple Message
+message= "hi there"
+print(message)
+
+# 2-2 Simple messages
+message= "hi part 2"
+print(message)
+message= "hi part 3"
+print(message)
+
+name= "ada.lovelace"
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+first_name="ada"
+last_name="lovelace"
+full_name=f"{first_name} {last_name}"
+print(full_name)
+
+message=f"Hello, {full_name.title()}!"
+print(message)
+
+print("python")
+print("\tpython")
+print("languages:\n\t python \n\t java")
+
+favorite_language= "  R  "
+print(favorite_language.rstrip())
+print(favorite_language)
+
+#favorite_language=favorite_language.rstrip()
+print(favorite_language.strip())
+print(favorite_language.lstrip())
+
+
+#2-3 Personal Message
+first_name="Steven"
+last_name="G"
+print(f"I love {first_name} {last_name}")
+
+#2-4 Name Cases
+full_name=f"{first_name} {last_name}"
+print(full_name.lower())
+print(full_name.title())
+print(full_name.upper())
+
+#2-5 Famous Quote
+print('Shakespeare once said,"To be or not to be \n that is the question"')
+
+#2-6 Famous Quote 2
+famous_person = "Shakespeare"
+message=f'{famous_person} once said, "To be or not to be \n that is the question"'
+print(message)
+
+#2-7 Stripping Names
+name_thing="\t billy bob   \n\t joe   bob  "
+print(name_thing.strip())
+print(name_thing.rstrip())
+print(name_thing.lstrip())
+
+#2-8 Number Eight
+print(4+4)
+print(16/2)
+print(4*2)
+print(10-2)
+
+#2-9 Favorite number
+fav_num=1
+print(f"my favorite number is {fav_num}")
+
+#2-10 Adding Comments
+#done
+
+
+#2-11 zen of python
+#done
